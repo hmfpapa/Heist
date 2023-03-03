96ce4895f42080880d0ac1493b7a6ebe27c26e33
Hannah Papa <hmfloyd16@gmail.com>
2023-03-03 11:45:46 -0600

    Phase1

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Heist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Plan Your Heist!");
+
+Console.WriteLine("Enter team member's name:");
+string nameResponse = Console.ReadLine().Trim();
+
+Console.WriteLine("What is team member's skill level? (0 to 100)");
+int skillResponse = int.Parse(Console.ReadLine().Trim());
+
+
+Console.WriteLine("How courageous is this team member? (0 to 2)");
+double courageResponse = double.Parse(Console.ReadLine().Trim());
+
+  string MemberInformation(TeamMember member){
+                return $"{member.Name} {member.Skill} {member.Courage}";
+            }
+
+TeamMember mem1 = new TeamMember(nameResponse, skillResponse, courageResponse);
+
+string information = MemberInformation(mem1);
+
+Console.WriteLine(information);
+        }
+    }
+}
---
+++ TeamMember.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace Heist
+{
+    class TeamMember
+    {
+ public TeamMember (string name, int skill, double courage)
+            {
+                Name = name;
+                Skill = skill;
+                Courage = courage;
+
+            }
+
+            public string Name {get; set;}
+            public int Skill {get; set;}
+            public double Courage {get; set;}
+
+           
+    }
+}
