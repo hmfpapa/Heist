57adeb4c9c13e60e71ecc358ab1f79a89e3a5a09
Hannah Papa <hmfloyd16@gmail.com>
2023-03-03 14:35:06 -0600

    Phase5

--- Program.cs
+++ Program.cs
@@ -8,65 +8,62 @@ namespace Heist
     {
         static void Main(string[] args)
         {
-            int luck = new Random().Next(-10, 11);
-
-            int bankDifficulty = 100 + luck;
-
             Console.WriteLine("Plan Your Heist!");
             Dictionary<string, TeamMember> team1 = new();
-            string response = "y";
-            while (response == "y")
+
+            string nameResponse = "NAME";
+
+            while (nameResponse != "")
             {
 
-                Console.WriteLine("Enter team member's name:");
-                string nameResponse = Console.ReadLine();
+                Console.WriteLine("Enter team member's name: (Press enter to stop entering names and start a trial run)");
+                nameResponse = Console.ReadLine();
 
                 if (nameResponse == "")
                 {
-                    Console.WriteLine($"Team count: {team1.Count}");
+                    continue;
+                }
 
-                    // foreach (KeyValuePair<string, TeamMember> t in team1)
-                    // {
-                    //     Console.WriteLine(t.Value.MemberInformation());
-                    // }
-                    break;
+                Console.WriteLine($"What is {nameResponse}'s skill level? (0 to 100)");
+                int skillResponse = int.Parse(Console.ReadLine().Trim());
+
+                Console.WriteLine($"How courageous is {nameResponse}? (0 to 2)");
+                double courageResponse = double.Parse(Console.ReadLine().Trim());
+
+                TeamMember newMember = new TeamMember(nameResponse, skillResponse, courageResponse);
+
+                // Console.WriteLine(newMember.MemberInformation());
+
+                team1.Add($"{newMember.Name}", newMember);
+
+            }
+
+            Console.WriteLine("How Many trial runs would you like to perform?");
+            int trialRuns = int.Parse(Console.ReadLine());
+
+
+            for (int i = 1; i <= trialRuns; i++)
+            {
+                int luck = new Random().Next(-10, 11);
+                int bankDifficulty = 100 + luck;
+                int teamSkillLevel = team1.Sum(x => x.Value.Skill);
+
+                Console.WriteLine($"Your current team skill level is {teamSkillLevel}. The bank difficulty is {bankDifficulty}");
+
+                if (teamSkillLevel < bankDifficulty)
+                {
+                    Console.WriteLine("You have failed");
                 }
                 else
                 {
-
-
-                    Console.WriteLine($"What is {nameResponse}'s skill level? (0 to 100)");
-                    int skillResponse = int.Parse(Console.ReadLine().Trim());
-
-                    Console.WriteLine($"How courageous is {nameResponse}? (0 to 2)");
-                    double courageResponse = double.Parse(Console.ReadLine().Trim());
-
-                    TeamMember newMember = new TeamMember(nameResponse, skillResponse, courageResponse);
-
-                    // Console.WriteLine(newMember.MemberInformation());
-
-                    team1.Add($"{newMember.Name}", newMember);
-
-                    int teamSkillLevel = team1.Sum(x => x.Value.Skill);
-
-                    Console.WriteLine($"Your current team skill level is {teamSkillLevel}. The bank difficulty is {bankDifficulty}");
-
-                    if (teamSkillLevel < bankDifficulty)
-                    {
-                        Console.WriteLine("You have failed");
-                    }
-                    else
-                    {
-                        Console.WriteLine("You have succeeded!");
-                    }
-
-                    Console.WriteLine("Would you like to enter another new team member? Y/N:");
-                    response = Console.ReadLine().ToLower();
-
+                    Console.WriteLine("You have succeeded!");
                 }
             }
 
         }
     }
+
 }
+
+
 
