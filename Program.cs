85386b5cd63ed5a07bfb33f0c3b2b2199eaf88c5
Hannah Papa <hmfloyd16@gmail.com>
2023-03-03 13:49:12 -0600

    Phase2

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Heist
 {
@@ -7,26 +8,45 @@ namespace Heist
         static void Main(string[] args)
         {
             Console.WriteLine("Plan Your Heist!");
+            Dictionary<string, TeamMember> team1 = new();
+            string response = "y";
+            while (response == "y")
+            {
 
-Console.WriteLine("Enter team member's name:");
-string nameResponse = Console.ReadLine().Trim();
+                Console.WriteLine("Enter team member's name:");
+                string nameResponse = Console.ReadLine();
 
-Console.WriteLine("What is team member's skill level? (0 to 100)");
-int skillResponse = int.Parse(Console.ReadLine().Trim());
+                if (nameResponse == "")
+                {
+                    Console.WriteLine($"Team count: {team1.Count}");
 
+                    foreach (KeyValuePair<string, TeamMember> t in team1)
+                    {
+                        Console.WriteLine(t.Value.MemberInformation());
+                    }
+                    break;
+                }
+                else
+                {
+            Console.WriteLine("What is team member's skill level? (0 to 100)");
+            int skillResponse = int.Parse(Console.ReadLine().Trim());
 
-Console.WriteLine("How courageous is this team member? (0 to 2)");
-double courageResponse = double.Parse(Console.ReadLine().Trim());
+            Console.WriteLine("How courageous is this team member? (0 to 2)");
+            double courageResponse = double.Parse(Console.ReadLine().Trim());
 
-  string MemberInformation(TeamMember member){
-                return $"{member.Name} {member.Skill} {member.Courage}";
+            TeamMember newMember = new TeamMember(nameResponse, skillResponse, courageResponse);
+
+            Console.WriteLine(newMember.MemberInformation());
+
+                team1.Add($"{newMember.Name}", newMember);
+
+          Console.WriteLine("Would you like to enter another new team member? Y/N:");
+          response = Console.ReadLine().ToLower();
+
+                }
             }
 
-TeamMember mem1 = new TeamMember(nameResponse, skillResponse, courageResponse);
-
-string information = MemberInformation(mem1);
-
-Console.WriteLine(information);
         }
     }
 }
+
